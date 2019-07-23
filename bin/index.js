037da3aa621ea951b96e2ca45f29e7aff1ac8ec1
lidandan-pd <lidandan-pd@360.cn>
2019-07-23 16:39:24 +0800

    项目文件配置

--- bin/index.js
+++ bin/index.js
@@ -1,77 +1,83 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const inquirer = require('inquirer');
-const chalk = require('chalk');
-const ora = require('ora');
-const path = require('path');
-const exists = require('fs').existsSync;
+const program = require('commander')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const path = require('path')
+const exists = require('fs').existsSync
 const Generate = require('../lib/generator')
+const execa = require('execa')
 
 program
-    .version(require('../package').version, '-v, --version')
-    .usage('<project-name>');
+  .version(require('../package').version, '-v, --version')
+  .usage('<project-name>')
 
 program.on('--help', () => {
-    console.log();
-    console.log('  Examples:')
-    console.log()
-    console.log(chalk.gray('    # create a new project with an simple template'))
-    console.log('    $ baike my-project')
-    console.log()
-    });
-
-function help () {
-    program.parse(process.argv)
-    if (program.args.length < 1) return program.help()
-}
-help();
-
-if (program.args) {
-    const rawName = program.args[0]
-    const projectPath = path.resolve(rawName);
-    inquirer.prompt([{
-        type: 'confirm',
-        message: exists(projectPath) 
-            ? 'Target directory exists. Continue?'
-            : 'Generate project in current directory?',
-        name: 'ok'
-    }]).then(answers => {
-        if (answers.ok) {
-            run(projectPath,rawName)
-        }
-    }).catch(e => {
-        throw new Error(e);
-    })
-}
-
+  console.log()
+  console.log('  Examples:')
+  console.log()
+  console.log(chalk.gray('    # create a new project with an simple template'))
+  console.log('    $ baike my-project')
+  console.log()
+})
 process.on('exit', () => {
-    console.log()
+  console.log()
 })
 
-function run(projectPath,projectName) {
-    const plugins = new Set();
-    inquirer.prompt([{
-        type: 'confirm',
-        message: '是否使用axio进行数据请求？',
-        name: 'ok'
-    }]).then(answers => {
-        if (answers.ok) {
-            // 加入axios代码部分
-           plugins.add('axios');
-        } 
-        inquirer.prompt([{
-            type: 'confirm',
-            message: '是否使用cdn',
-            name: 'ok'
-        }]).then(answers => {
-            if (answers.ok) {
-                // 加入cdn代码部分
-            //plugins.add('cdn');
-            } 
-            const generator = new Generate(plugins, projectPath, projectName)
-            generator.create();
-        })
+async function create () {
+  program.parse(process.argv)
+  if (program.args.length < 1) return program.help()
+
+  const rawName = program.args[0]
+  const projectPath = path.resolve(rawName)
+  const isExist = exists(projectPath)
+  const { ok } = await inquirer.prompt([{
+    type: 'confirm',
+    message: isExist
+      ? chalk.red('Target directory exists. Continue?')
+      : 'Generate project in current directory?',
+    name: 'ok'
+  }])
+  if (ok) {
+    if (isExist) {
+      const child = execa('rm', ['-r', projectPath])
+      child.on('close', code => {
+      })
+    }
+    run(projectPath, rawName)
+  }
+}
+
+create()
+
+const plugins = new Set()
+async function run (projectPath, projectName) {
+  prompt({
+    type: 'confirm',
+    message: '是否使用axio进行数据请求？',
+    name: 'ok',
+    plugin: 'axios'
+  }, () => {
+    prompt({
+      type: 'confirm',
+      message: '是否使用qcdn',
+      name: 'ok',
+      plugin: 'qcdn'
+    }, () => {
+      const generator = new Generate(plugins, projectPath, projectName)
+      generator.create()
     })
-    
+  })
+}
+
+async function prompt ({ type, message, name, plugin }, callback) {
+  const { ok } = await inquirer.prompt([{
+    type: type,
+    message: message,
+    name: name
+  }])
+  if (ok) {
+    plugins.add(plugin)
+  }
+  callback && callback()
 }
--- lib/template/src/App.vue
+++ lib/template/src/App.vue
@@ -1,28 +1,3 @@
-<%_ if (!rootOptions.router) { _%>
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <%_ if (!rootOptions.bare) { _%>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <%_ } else { _%>
-    <h1>Welcome to Your Vue.js App</h1>
-    <%_ } _%>
-  </div>
-</template>
-  <%_ if (!rootOptions.bare) { _%>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-  <%_ } _%>
-<%_ } else { _%>
 <template>
   <div id="app">
     <div id="nav">
@@ -32,31 +7,22 @@ export default {
     <router-view/>
   </div>
 </template>
-<%_ } _%>
-<%_ if (!rootOptions.bare) { _%>
-<%_ if (rootOptions.cssPreprocessor !== 'stylus') { _%>
+<script>
+export default {
+  
+}
+</script>
 
-<style<%-
-  rootOptions.cssPreprocessor
-    ? ` lang="${
-        rootOptions.cssPreprocessor.includes('sass')
-          ? 'scss'
-          : rootOptions.cssPreprocessor
-      }"`
-    : ``
-%>>
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  <%_ if (!rootOptions.router) { _%>
   margin-top: 60px;
-  <%_ } _%>
 }
-<%_ if (rootOptions.router) { _%>
-  <%_ if (!rootOptions.cssPreprocessor) { _%>
+
 #nav {
   padding: 30px;
 }
@@ -69,41 +35,4 @@ export default {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-  <%_ } else { _%>
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-  <%_ } _%>
-<%_ } _%>
 </style>
-<%_ } else { _%>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  <%_ if (!rootOptions.router) { _%>
-  margin-top 60px
-  <%_ } _%>
-<%_ if (rootOptions.router) { _%>
-
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
-<%_ } _%>
-</style>
-<%_ } _%>
-<%_ } _%>
