0277255a140bfd6b28620bf01be5adf6f73a3734
lidandan-pd <lidandan-pd@360.cn>
2019-08-07 19:12:26 +0800

    login

---
+++ lib/packages/com-domain-login/template/login.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-popover
+    placement="bottom"
+    width="60"
+    trigger="hover">
+        <a @click="checkout" class="checkout">退出</a>
+        <a slot="reference">{{username}}</a>
+    </el-popover>
+</template>
+<script>
+export default {
+  name: 'comlogin',
+  data() {
+    username: 'xiaoqi'
+  },
+  methods: {
+    checkout() {
+      
+    }
+  }
+}
+</script>
